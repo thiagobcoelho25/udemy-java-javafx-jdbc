9625f78b78e9b6cf724c151c90436463e908a41f
thiagobcoelho25 <thiagobcoelho25@gmail.com>
2021-01-15 16:09:44 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -97,5 +97,12 @@ public class Utils {
 		});
 	}
 
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 	
 }
